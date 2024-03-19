efd8e0e3596ac36f90bee9713fb99ef1079184db
Ayumu <hrtaym1114@gmail.com>
2024-03-20 05:56:16 +0900

    3月20日推進1

--- pages/simple_chat.py
+++ pages/simple_chat.py
@@ -1,9 +1,17 @@
 import streamlit as st
+from openai import OpenAI
 
 st.title("simple chat")
 
 user_message = st.text_input(label="どうしました？")
 
 if user_message:
-    ai_message = "こんにちは！"
-    st.write(ai_message)
+    client = OpenAI()
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": user_message},
+        ],
+    )
+    st.write(completion)
